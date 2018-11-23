eed0b57c234a3fb98a695f7722380e080d6f8bcc
leftthomas <leftthomas@qq.com>
2018-11-23 16:47:58 +0800

    revert model

--- model.py
+++ model.py
@@ -11,14 +11,14 @@ class Model(nn.Module):
         upsample_block_num = int(math.log(upscale_factor, 2))
         if upscale_factor % 2 == 0:
             upscale_factor = 2
-        self.block1 = CapsuleConv2d(3, 64, 9, 1, 4, padding=4, similarity='tonimoto')
-        self.block2 = CapsuleConv2d(64, 64, 3, 4, 8, padding=1, similarity='tonimoto')
-        self.block3 = CapsuleConv2d(64, 64, 3, 8, 8, padding=1, similarity='tonimoto')
-        self.block4 = CapsuleConv2d(64, 64, 3, 8, 16, padding=1, similarity='tonimoto')
-        self.block5 = CapsuleConv2d(64, 64, 3, 16, 16, padding=1, similarity='tonimoto')
-        self.block6 = CapsuleConv2d(64, 64, 3, 16, 8, padding=1, similarity='tonimoto')
-        self.block7 = CapsuleConv2d(64, 64, 3, 8, 8, padding=1, similarity='tonimoto')
-        self.block8 = CapsuleConv2d(64, 64, 3, 8, 4, padding=1, similarity='tonimoto')
+        self.block1 = CapsuleConv2d(3, 64, 9, 1, 4, padding=4, similarity='tonimoto', squash=False)
+        self.block2 = CapsuleConv2d(64, 64, 3, 4, 8, padding=1, similarity='tonimoto', squash=False)
+        self.block3 = CapsuleConv2d(64, 64, 3, 8, 8, padding=1, similarity='tonimoto', squash=False)
+        self.block4 = CapsuleConv2d(64, 64, 3, 8, 16, padding=1, similarity='tonimoto', squash=False)
+        self.block5 = CapsuleConv2d(64, 64, 3, 16, 16, padding=1, similarity='tonimoto', squash=False)
+        self.block6 = CapsuleConv2d(64, 64, 3, 16, 8, padding=1, similarity='tonimoto', squash=False)
+        self.block7 = CapsuleConv2d(64, 64, 3, 8, 8, padding=1, similarity='tonimoto', squash=False)
+        self.block8 = CapsuleConv2d(64, 64, 3, 8, 4, padding=1, similarity='tonimoto', squash=False)
         self.block9 = nn.Sequential(*[UpsampleBlock(64, upscale_factor, 4, 4) for _ in range(upsample_block_num)])
         self.block10 = CapsuleConv2d(64, 3, 9, 4, 1, padding=4, similarity='tonimoto')
 
@@ -40,12 +40,10 @@ class UpsampleBlock(nn.Module):
     def __init__(self, in_channels, upscale_factor, in_length, out_length):
         super(UpsampleBlock, self).__init__()
         self.conv = CapsuleConv2d(in_channels, in_channels * upscale_factor ** 2, 3, in_length, out_length, padding=1,
-                                  similarity='tonimoto')
+                                  similarity='tonimoto', squash=False)
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
-        self.prelu = nn.PReLU()
 
     def forward(self, x):
         x = self.conv(x)
         x = self.pixel_shuffle(x)
-        x = self.prelu(x)
         return x
