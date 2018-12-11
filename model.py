e54f79daf3be77f5123f2e66853030ab1a8a2abb
leftthomas <leftthomas@qq.com>
2018-12-11 13:41:12 +0800

    change model

--- model.py
+++ model.py
@@ -5,10 +5,12 @@ class InConv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(InConv, self).__init__()
         self.conv = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-        self.relu = nn.LeakyReLU(negative_slope=0.2)
+        self.bn = nn.BatchNorm2d(out_ch)
+        self.relu = nn.PReLU()
 
     def forward(self, x):
         x = self.conv(x)
+        x = self.bn(x)
         x = self.relu(x)
         return x
 
@@ -16,14 +18,20 @@ class InConv(nn.Module):
 class DownConv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(DownConv, self).__init__()
-        self.conv = nn.Conv2d(in_ch, out_ch, 3, stride=2, padding=1)
-        self.bn = nn.BatchNorm2d(out_ch)
-        self.relu = nn.LeakyReLU(negative_slope=0.2)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_ch)
+        self.relu1 = nn.PReLU()
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+        self.relu2 = nn.PReLU()
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
         return x
 
 
@@ -31,20 +39,8 @@ class Transform(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(Transform, self).__init__()
         self.conv = nn.Conv2d(in_ch, out_ch, 1)
-        self.relu = nn.LeakyReLU(negative_slope=0.2)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.relu(x)
-        return x
-
-
-class UpConv(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(UpConv, self).__init__()
-        self.conv = nn.ConvTranspose2d(in_ch, out_ch, 3, stride=2, padding=1, output_padding=1)
         self.bn = nn.BatchNorm2d(out_ch)
-        self.relu = nn.LeakyReLU(negative_slope=0.2)
+        self.relu = nn.PReLU()
 
     def forward(self, x):
         x = self.conv(x)
@@ -53,14 +49,36 @@ class UpConv(nn.Module):
         return x
 
 
+class UpConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(UpConv, self).__init__()
+        self.conv1 = nn.ConvTranspose2d(in_ch, out_ch, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_ch)
+        self.relu1 = nn.PReLU()
+        self.conv2 = nn.ConvTranspose2d(out_ch, out_ch, 3, stride=2, padding=1, output_padding=1)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+        self.relu2 = nn.PReLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        return x
+
+
 class OutConv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(OutConv, self).__init__()
         self.conv = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+        self.bn = nn.BatchNorm2d(out_ch)
         self.tanh = nn.Tanh()
 
     def forward(self, x):
         x = self.conv(x)
+        x = self.bn(x)
         x = self.tanh(x)
         return (x + 1) / 2
 
@@ -72,23 +90,20 @@ class Model(nn.Module):
         self.in_c = InConv(3, 64)
         self.down1 = DownConv(64, 128)
         self.down2 = DownConv(128, 256)
-        self.down3 = DownConv(256, 256)
-        self.down4 = DownConv(256, 512)
-        self.down5 = DownConv(512, 512)
+        self.down3 = DownConv(256, 512)
+        self.down4 = DownConv(512, 512)
 
         self.transform_t = Transform(512, 512)
         self.transform_r = Transform(512, 512)
 
-        self.up5_t = UpConv(512, 512)
-        self.up4_t = UpConv(512, 256)
-        self.up3_t = UpConv(256, 256)
+        self.up4_t = UpConv(512, 512)
+        self.up3_t = UpConv(512, 256)
         self.up2_t = UpConv(256, 128)
         self.up1_t = UpConv(128, 64)
         self.out_t = OutConv(64, 3)
 
-        self.up5_r = UpConv(512, 512)
-        self.up4_r = UpConv(512, 256)
-        self.up3_r = UpConv(256, 256)
+        self.up4_r = UpConv(512, 512)
+        self.up3_r = UpConv(512, 256)
         self.up2_r = UpConv(256, 128)
         self.up1_r = UpConv(128, 64)
         self.out_r = OutConv(64, 3)
@@ -100,23 +115,20 @@ class Model(nn.Module):
         x_d2 = self.down2(x_d1)
         x_d3 = self.down3(x_d2)
         x_d4 = self.down4(x_d3)
-        x_d5 = self.down5(x_d4)
 
         # transform
-        out_t = self.transform_t(x_d5)
-        out_r = self.transform_r(x_d5)
+        out_t = self.transform_t(x_d4)
+        out_r = self.transform_r(x_d4)
 
         # decoder of transmission
-        x_u5_t = self.up5_t(out_t)
-        x_u4_t = self.up4_t(x_u5_t)
+        x_u4_t = self.up4_t(out_t)
         x_u3_t = self.up3_t(x_u4_t)
         x_u2_t = self.up2_t(x_u3_t)
         x_u1_t = self.up1_t(x_u2_t)
         transmission = self.out_t(x_u1_t)
 
         # decoder of reflection
-        x_u5_r = self.up5_r(out_r)
-        x_u4_r = self.up4_r(x_u5_r)
+        x_u4_r = self.up4_r(out_r)
         x_u3_r = self.up3_r(x_u4_r)
         x_u2_r = self.up2_r(x_u3_r)
         x_u1_r = self.up1_r(x_u2_r)
