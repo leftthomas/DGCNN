7e917fd935a8f8cf58d9335df65c1a7d3f8a5f8b
leftthomas <leftthomas@qq.com>
2019-03-18 20:24:11 +0800

    remove unused code; change model

--- model.py
+++ model.py
@@ -1,22 +1,24 @@
 import torch
-from capsule_layer import CapsuleLinear
+import torch.functional as F
 from torch import nn
-from torch_geometric.nn import GCNConv
-
-from utils import global_sort_pool
+from torch.nn import Conv1d, MaxPool1d, Linear, Dropout
+from torch_geometric.nn import GCNConv, global_sort_pool
 
 
 class Model(nn.Module):
-    def __init__(self, num_features, num_classes, num_iterations=3):
+    def __init__(self, num_features, num_classes):
         super(Model, self).__init__()
 
         self.conv1 = GCNConv(num_features, 32)
         self.conv2 = GCNConv(32, 32)
         self.conv3 = GCNConv(32, 32)
         self.conv4 = GCNConv(32, 1)
-        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=97, out_length=16, in_capsules=None,
-                                        share_weight=True, routing_type='k_means', similarity='tonimoto',
-                                        num_iterations=num_iterations)
+        self.conv5 = Conv1d(97, 16, 97, 97)
+        self.conv6 = Conv1d(16, 32, 5, 1)
+        self.pool = MaxPool1d(2, 2)
+        self.classifier = Linear(128, num_classes)
+        self.drop_out = Dropout(0.5)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
@@ -26,8 +28,13 @@ class Model(nn.Module):
         x_3 = torch.tanh(self.conv3(x_2, edge_index))
         x_4 = torch.tanh(self.conv4(x_3, edge_index))
         x = torch.cat([x_1, x_2, x_3, x_4], dim=-1)
-        x = global_sort_pool(x, batch)
-        out = self.classifier(x)
-        classes = out.norm(dim=-1)
+        x = global_sort_pool(x, batch, k=50)
+        x = x.view(*x.size(), 1)
+        x = self.relu(self.conv5(x))
+        x = self.pool(x)
+        x = self.relu(self.conv6(x))
+        x = x.view(x.size(0), -1)
+        out = F.softmax(self.classifier(x), dim=-1)
+        classes = self.drop_out(out)
 
         return classes
--- utils.py
+++ utils.py
@@ -1,46 +1,5 @@
 import torch
-import torch.nn.functional as F
-from torch import nn
-from torch_geometric.utils import to_dense_batch, degree
-
-
-def global_sort_pool(x, batch, k=None):
-    r"""where node features are first sorted individually and then sorted in
-    descending order based on their last features. The first :math:`k` nodes
-    form the output of the layer, if k equals None, than don't select nodes.
-
-    Args:
-        x (Tensor): Node feature matrix
-            :math:`\mathbf{X} \in \mathbb{R}^{N \times F}`.
-        batch (LongTensor): Batch vector :math:`\mathbf{b} \in {\{ 0, \ldots,
-            B-1\}}^N`, which assigns each node to a specific example.
-        k (int): The number of nodes to hold for each graph.
-
-    :rtype: :class:`Tensor`
-    """
-    x, _ = x.sort(dim=-1)
-
-    fill_value = x.min().item() - 1
-    batch_x, num_nodes = to_dense_batch(x, batch, fill_value)
-    B, N, D = batch_x.size()
-
-    _, perm = batch_x[:, :, -1].sort(dim=-1, descending=True)
-    arange = torch.arange(B, dtype=torch.long, device=perm.device) * N
-    perm = perm + arange.view(-1, 1)
-
-    batch_x = batch_x.view(B * N, D)
-    batch_x = batch_x[perm]
-    batch_x = batch_x.view(B, N, D)
-
-    if k is not None and N >= k:
-        batch_x = batch_x[:, :k].contiguous()
-    elif k is not None:
-        expand_batch_x = batch_x.new_full((B, k - N, D), fill_value)
-        batch_x = torch.cat([batch_x, expand_batch_x], dim=1)
-
-    batch_x[batch_x == fill_value] = 0
-
-    return batch_x
+from torch_geometric.utils import degree
 
 
 class Indegree(object):
@@ -76,13 +35,3 @@ class Indegree(object):
     def __repr__(self):
         return '{}(norm={}, max_value={})'.format(self.__class__.__name__, self.norm, self.max)
 
-
-class MarginLoss(nn.Module):
-    def __init__(self):
-        super(MarginLoss, self).__init__()
-
-    def forward(self, classes, labels):
-        left = F.relu(0.9 - classes, inplace=True) ** 2
-        right = F.relu(classes - 0.1, inplace=True) ** 2
-        loss = labels * left + 0.5 * (1 - labels) * right
-        return loss.sum(dim=-1).mean()
